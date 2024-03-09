0ae26897f8ce9c9c4cdec21ace7bd99a4fc9b1f0
harkanni <harkanzyempire001@gmail.com>
2024-03-09 15:43:44 +0100

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,250 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import axios from 'axios';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Tooltip from '@mui/material/Tooltip';
+import Stack from '@mui/material/Stack';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import Session from '@/components/SessionWrapper';
+import SessionWrapper from '@/components/SessionWrapper';
+import Spacer from '@/components/Spacer';
+import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
+// import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+import { UserContext } from '@/constants/UserContext';
+import SendIcon from '@mui/icons-material/Send'
+import AddIcon from '@mui/icons-material/AddCircleRounded';
+import { dogBreeds } from '@/constants/dogBreeds';
+import Summary from '@/components/Summary';
+import dayjs, { Dayjs } from 'dayjs';
+import moment from 'moment';
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+// const dogBreed = ['dogBreed', 'dogBreed', 'dogBreed', 'dogBreed', 'dogBreed']
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+export default function CommonlyUsedComponents() {
+   const [sessionCount, setSessionCount] = React.useState(1);
+   const [date, setDate] = React.useState<any | null>(null);
+   const [time, setTime] = React.useState<any | null>(null);
+   const [user, setUser] = React.useState({ firstName: '', lastName: '', address: '', 'session': '' });
+   const [breed, setDogBreed] = React.useState('')
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+   const [loading, setLoading] = React.useState(false);
+   const [modal, setModal] = React.useState(false);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+   const [sessionData, setSessionData] = React.useState<any>([{ time: '', date: '' }]);
+   const [sessionState, setSessionState] = React.useState({ time: '', date: '' });
+
+
+
+
+   const pause = (time: number) =>
+      new Promise((resolve) => {
+         setTimeout(resolve, time);
+      });
+
+   const addSession = (index: any) => {
+      console.log('addSession index: ', index)
+      setSessionCount(sessionCount + 1);
+      console.log('This is time: ', sessionState.time, 'This is date : ', sessionState.date, 'This is session : ', sessionState)
+      let state = [...sessionData, sessionState]
+      setSessionData([...state])
+      setSessionState({ time: '', date: '' })
+      console.log('state: ', state)
+   };
+
+   const handleSetDogBreed = (event: SelectChangeEvent) => {
+      // console.log(event)
+      setDogBreed(event.target.value);
+   };
+
+   // DEMO SCHEDULE HANDLER
+   // const handleSchedule = async () => {
+   //    console.log('schedule creation started');
+
+   //    setLoading(true)
+
+   //    await pause(5000);
+
+   //    setLoading(false);
+
+   //    // await pause(1);
+
+   //    setModal(true);
+
+   //    console.log('schedule created');
+   //    console.log(sessionData)
+   // }
+
+   const handleSchedule = async () => {
+      setLoading(true);
+
+      // Assuming your API endpoint is '/api/schedule'
+
+      // const apiEndpoint = 'http://localhost:8080/api/v1/calender/createSchedule';
+      const apiEndpoint = 'https://alert-trench-coat-foal.cyclic.app/api/v1/calender/createSchedule';
+
+      try {
+         // Make a POST request to the API with the sessionData
+         const response = await axios.post(apiEndpoint, { ...user, dogBreed:breed, session:sessionData });
+
+         // Check if the request was successful (status code 2xx)
+         if (response.status === 200) {
+            setModal(true);
+            console.log('Schedule created successfully');
+         } else {
+            console.error('Failed to create schedule:', response.statusText);
+            // Handle error appropriately, e.g., show an error message
+         }
+      } catch (error: any) {
+         console.error('An error occurred while creating the schedule:', error.message);
+         // Handle error appropriately, e.g., show an error message
+      } finally {
+         setLoading(false);
+      }
+   };
+
+
+   const handleNameChange = (event: any) => {
+      const { name, value } = event.target;
+      setUser((prevData) => ({
+         ...prevData,
+         [name]: value,
+      }));
+   }
+
+   const handleDateChange = (date: any, id: any) => {
+      console.log(id, moment(date).format('ddd. MMMM Do, YYYY'));
+
+      // Update sessionState
+      const updatedSessionState = { ...sessionState, date: moment(date).format('ddd. MMMM Do, YYYY') };
+      setSessionState(updatedSessionState);
+
+      // IF UPDATING SESSION DATA
+      if (sessionData[id]) {
+         // Update the specific session in the sessionData array
+         const updatedSessionData = sessionData.map((session: any, index: any) =>
+            index === id ? { ...session, date: moment(date).format('ddd. MMMM Do, YYYY') } : session
+         );
+
+         console.log("This is edited session: ", updatedSessionData[id]);
+         setSessionData(updatedSessionData);
+      }
+   };
+
+   const handleTimeChange = (time: any, id: any) => {
+      console.log(id, moment(time).format('HH:mm a'));
+
+      // Update sessionState
+      const updatedSessionState = { ...sessionState, time: moment(time).format('HH:mm a') };
+      setSessionState(updatedSessionState);
+
+      // IF UPDATING SESSION DATA
+      if (sessionData[id]) {
+         // Update the specific session in the sessionData array
+         const updatedSessionData = sessionData.map((session: any, index: any) =>
+            index === id ? { ...session, time: moment(time).format('HH:mm a') } : session
+         );
+
+         console.log("This is edited session: ", updatedSessionData[id]);
+         setSessionData(updatedSessionData);
+      }
+   };
+
+   const handleDateOrTimeChange = (value: any, field: any, id: any) => {
+      console.log("Date: ", value, " Field: ", field)
+      const formattedValue =
+         field === 'date'
+            ? moment(value).format('ddd. MMMM Do, YYYY')
+            : moment(value).format('HH:mm a');
+
+      setSessionState((prevState) => ({ ...prevState, [field]: formattedValue }));
+
+      if (sessionData[id]) {
+         const updatedSessionData = sessionData.map((session: any, index: any) =>
+            index === id ? { ...session, [field]: formattedValue } : session
+         );
+         setSessionData(updatedSessionData);
+      }
+   };
+
+
+   return (
+      <UserContext.Provider value={[]}>
+         <LocalizationProvider dateAdapter={AdapterMoment}>
+            <div className={`${(loading || modal) && 'blurBg'} form-container flex flex-col gap-4 min-w-[70vw] min-h-[70vh] m-auto bg-white rounded-md`}>
+               {
+                  [...Array(sessionCount)].map((_, index) => {
+                     // console.log(index)
+                     return (
+                        <SessionWrapper key={`${index} session`} id={index}>
+                           <DatePicker localeText={{ clockLabelText: () => '' }} disablePast format='ddd. MMMM Do, YYYY' label="Session day" className='flex-1' value={date} onChange={(date: any, id) => handleDateOrTimeChange(date, 'date', index)} name={'date'} />
+                           <Spacer />
+                           <TimePicker label="Start time" className='flex-1' onChange={(time: any) => handleDateOrTimeChange(time, 'time', index)} name='time' value={time} />
+                           <Button className='hover:bg-slate-500 flex justify-center' variant="contained" onClick={() => addSession(index)}>
+                              <AddIcon color='action' />
+                           </Button>
+                        </SessionWrapper>
+                     )
+                  })
+               }
+
+               <SessionWrapper>
+                  <TextField id="outlined-basic" label="First name" name='firstName' variant="outlined" className='flex-1' value={user.firstName} onChange={handleNameChange} />
+                  <Spacer />
+                  <TextField id="outlined-basic" label="Last name" name='lastName' variant="outlined" className='flex-1' value={user.lastName} onChange={handleNameChange} />
+               </SessionWrapper>
+
+               <SessionWrapper>
+                  <FormControl fullWidth>
+                     <InputLabel id="demo-simple-select-label">Select Dog Breed 🐶</InputLabel>
+                     <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={breed}
+                        label="Select Dog Breed 🐶"
+                        onChange={(event) => handleSetDogBreed(event)}
+                     >
+                        {dogBreeds.map((dogBreed: string, index: number) => {
+                           return (
+                              <MenuItem key={index} value={dogBreed}>{dogBreed}</MenuItem>
+                           )
+                        })}
+                     </Select>
+                  </FormControl>
+               </SessionWrapper>
+
+               <SessionWrapper>
+                  <TextField
+                     id="outlined-multiline-flexible"
+                     label="Address"
+                     multiline
+                     maxRows={4}
+                     minRows={4}
+                     fullWidth
+                     name='address'
+                     value={user.address}
+                     onChange={handleNameChange}
+                  />
+               </SessionWrapper>
+
+               <Button className='text-black text-bold p-4 w-[40%] hover:bg-slate-500' variant="contained" endIcon={<SendIcon />} onClick={handleSchedule}>
+                  Schedule
+               </Button>
+
+
+            </div>
+            {
+               (loading || modal) && <Summary setModal={setModal} user={user} dogBreed={breed} sessionCount={sessionCount} loading={loading} setLoading={setLoading} />
+            }
+
+         </LocalizationProvider>
+
+      </UserContext.Provider>
+   );
 }
