6c2f39aa3ae465040db269f47091a46d741c9061
harkanni <harkanzyempire001@gmail.com>
2024-03-19 10:08:53 +0100

    main section

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -1,10 +1,61 @@
+'use client'
+
+
 import React from 'react'
 import styles from '@/app/dashboard.module.css'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers';
+import moment from 'moment';
 
 const MainSection = () => {
-  return (
-    <div className={`flex-[40rem] p-4 ${styles['box-shadow']}`}>MainSection</div>
-  )
+   const [sessionCount, setSessionCount] = React.useState(1);
+   const [date, setDate] = React.useState<any | null>(null);
+   const [time, setTime] = React.useState<any | null>(null);
+   const [user, setUser] = React.useState({ firstName: '', lastName: '', address: '', 'session': '' });
+   const [breed, setDogBreed] = React.useState('')
+
+   const [loading, setLoading] = React.useState(false);
+   const [modal, setModal] = React.useState(false);
+
+   const [sessionData, setSessionData] = React.useState<any>([{ time: '', date: '' }]);
+   const [sessionState, setSessionState] = React.useState({ time: '', date: '' });
+
+
+
+   const handleDateOrTimeChange = (value: any, field: any, id: any) => {
+      console.log("Date: ", value, " Field: ", field)
+      const formattedValue =
+         field === 'date'
+            ? moment(value).format('ddd. MMMM Do, YYYY')
+            : moment(value).format('HH:mm a');
+
+      setSessionState((prevState) => ({ ...prevState, [field]: formattedValue }));
+
+      if (sessionData[id]) {
+         const updatedSessionData = sessionData.map((session: any, index: any) =>
+            index === id ? { ...session, [field]: formattedValue } : session
+         );
+         setSessionData(updatedSessionData);
+      }
+   };
+
+
+
+   return (
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+         <div className={`flex-[40rem] p-10 ${styles['box-shadow']}`}>
+            <h1 className='mb-2'>Set Date Available</h1>
+            <DatePicker localeText={{ clockLabelText: () => '' }} disablePast format='ddd. MMMM Do, YYYY' label="" className='flex-1' value={date} onChange={(date: any, id) => handleDateOrTimeChange(date, 'date', id)} name={'date'} />
+         </div>
+
+         <div className='flex justify-between'>
+            <span>Overview</span>
+            <span>Today</span>
+         </div>
+
+      </LocalizationProvider>
+   )
 }
 
 export default MainSection
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import styles from '@/app/dashboard.module.css'
+
+import { RiDashboardFill } from "react-icons/ri";
+import { FaUserAlt } from "react-icons/fa";
+import { SlCalender } from "react-icons/sl";
+
+
 const Sidebar = () => {
-  return (
-    <section className={`flex-[2rem] p-4 ${styles['box-shadow']}`}>
-      <div>Dashboard</div>
-      <div>User</div>
-      <div>Calender</div>
-    </section>
-  )
+   return (
+      <section className={`flex flex-col gap-10 flex-[2rem] p-10 ${styles['box-shadow']}`}>
+         <div className='flex items-center gap-3'>
+            <RiDashboardFill size={25} />
+            Dashboard
+         </div>
+         <div className='flex items-center gap-3'>
+            <FaUserAlt size={25}/>
+            User
+         </div>
+         <div className='flex items-center gap-3'>
+            <SlCalender size={25}/>
+            Calender
+         </div>
+      </section>
+   )
 }
 
 export default Sidebar
